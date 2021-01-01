e46d6deec108a6024c720e09241c4a2ebc678beb
Nikhil Da Rocha <niks77883@gmail.com>
2021-01-02 00:27:02 +0530

    refactoring decimal to octal

--- numeric_conversion/decimal_to_octal.c
+++ numeric_conversion/decimal_to_octal.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
 
-  int decimal_number = 16;
+void decimal_to_octal(int decimal_number)
+{
   
   int remainder, quotient;
-  
   int index =0;
-  
   int arr[100];
   
-  int length = sizeof(arr)/sizeof(arr[0]);
-  
-  printf("The length of the array is %d\n", length);
-  
 
-  
   while(decimal_number != 0) {
   
     remainder = decimal_number%8;
@@ -25,17 +17,25 @@ int main()
     index += 1;
   }
   
-  printf("The remainder is %d\n", remainder);
-  printf("The 0th position of the array is %d\n", arr[0]);
-  printf("The 1st position of the array is %d\n", arr[1]);
-  
-  
+ 
   for(int i = index -1; i >0; i--)
   {
-    printf("The Octal number is %d ",arr[i]);
+    printf("The Octal number is %d\n",arr[i]);
   }
-   
+  
+}
+
+int main()
+{  
+  
+  int decimal_number;
+  printf("Enter a decimal number\n");
+  scanf("%d", &decimal_number);
+  decimal_to_octal(decimal_number);
+  
   return 0;
   
 }
+
+
 
