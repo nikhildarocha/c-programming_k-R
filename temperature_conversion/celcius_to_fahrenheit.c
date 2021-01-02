c901856124ad270efb19d315b90c53269292c063
Nikhil Da Rocha <niks77883@gmail.com>
2021-01-02 19:45:11 +0530

    correcting typo

--- temperature_conversion/celcius_to_fahrenheit.c
+++ temperature_conversion/celcius_to_fahrenheit.c
@@ -7,8 +7,8 @@ int main()
   int lower = 0, upper = 300, step = 20;
   
   celcius = lower;
-  printf("Fahrenheit To Celcius\n");
-  while(fahr <= upper) {
+  printf("Celcius To Fahrenheit\n");
+  while(celcius <= upper) {
     fahr = (9.0*(celcius)/ 5.0)+32;
     printf("%3.0f\t%6.1f\n", celcius, fahr);
     celcius = celcius + step;
