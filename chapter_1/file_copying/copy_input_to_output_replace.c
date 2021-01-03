d936684e699425f791cebdfab687ef79c511a8f0
Nikhil Da Rocha <niks77883@gmail.com>
2021-01-04 00:20:19 +0530

    copy input to output and replace spaces

---
+++ chapter_1/file_copying/copy_input_to_output_replace.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define NONBLANK 'a'
+
+int main()
+{
+  int c, lastc;
+  
+  lastc = NONBLANK;
+  
+  while((c = getchar()) != EOF) {
+    if (c != ' ') {
+      putchar(c);
+    }
+    if (c == ' ') {
+      if (lastc != ' ') {
+        putchar(c);
+      }
+    }
+    lastc = c;
+ }
+ 
+ return 0;
+
+}
