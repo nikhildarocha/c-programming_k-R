8b685ae8f9abf0d7c1c141259323b138be670579
Nikhil Da Rocha <niks77883@gmail.com>
2021-01-04 12:11:50 +0530

    using or in replace prog

---
+++ chapter_1/file_copying/copy_input_to_output_replace_v3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define NONBLANK 'a'
+
+int main()
+{
+  int c, lastc;
+  
+  lastc = NONBLANK;
+  while((c = getchar()) != EOF) {
+    if( c != ' ' || lastc != ' ')
+      putchar(c);
+    
+    lastc = c;
+  }
+  
+  return 0;
+}
+ 
