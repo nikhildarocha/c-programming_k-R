bce17510c11f106d84961a70b536fdef2b282723
Nikhil Da Rocha <niks77883@gmail.com>
2021-01-05 10:29:09 +0530

    print input one word per line

--- chapter_1/word_counting/print_input_one_word_per_line.c
+++ chapter_1/word_counting/print_input_one_word_per_line.c
@@ -5,4 +5,21 @@
 
 int main()
 {
+  int c, state;
+  
+  state = OUT;
+  while ((c = getchar()) != EOF) {
+    if ( c == ' ' || c == '\n' || c == '\t') {
+      if (state == IN) {
+        putchar('\n');
+        state = OUT;
+      }
+    }
+    else if (state == OUT) {
+      state = IN;
+      putchar(c);
+    }
+    else
+      putchar(c);
+  }
 }
